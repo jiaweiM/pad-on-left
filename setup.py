90055692c9edcdef92ce35bef993f25d8f5a1464
JiaweiMao <jiaweiM_philo@hotmail.com>
2017-10-03 22:40:19 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pad-on-left',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='Apache 2.0',
+    author='JiaweiMao',
+    author_email='',
+    description=''
+)
